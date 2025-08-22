340cba1ed50dcbd52fe897fa6805786cd2003330
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-08-22 18:00:14 +0100

    add some more tests

---
+++ tests/mandatory/main.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 20:07:56 by mait-all          #+#    #+#             */
+/*   Updated: 2025/08/18 16:05:13 by mdahani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+
+int	ft_destroy_window(t_mlx_data *mlx)
+{
+	mlx_destroy_window(mlx->mlx_ptr, mlx->mlx_window);
+	mlx_destroy_display(mlx->mlx_ptr);
+	free(mlx->mlx_ptr);
+	exit (0);
+}
+
+int main(int ac, char **av)
+{
+	t_mlx_data	mlx;
+	t_map		map;
+
+	// check file name
+	if (ac != 2 || !check_file_name(av[1]))
+		return (custom_error("Error:\nNo such file or directory!\n"), 1);
+
+	// check the map
+	ft_bzero(&map, sizeof(t_map));
+	if (!check_map(av[1], &map))
+		return (custom_error("Error:\nInvalid map!\n"), 1);
+	
+	// render_wall_texture(&mlx);
+	ft_bzero(&mlx, sizeof(t_mlx_data));
+	init_player(&mlx);
+	mlx.mlx_ptr = mlx_init();
+	mlx.mlx_window = mlx_new_window(mlx.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "mlx window");
+	if (!load_wall_texture(&mlx, "./textures/tile39.xpm"))
+	{
+		printf("Error: failed to load textures\n");
+		exit (1);
+	}
+	mlx_hook(mlx.mlx_window, 2, 1L << 0, key_pressed, &mlx);
+	mlx_hook(mlx.mlx_window, 3, 1L << 1, key_released, &mlx);
+	// mlx_hook(mlx.mlx_window, 3, 1L << 1, key_Released, &mlx);
+	mlx_loop_hook(mlx.mlx_ptr, update, &mlx);
+	mlx_hook(mlx.mlx_window, 17, 0, ft_destroy_window, &mlx);
+	mlx_loop(mlx.mlx_ptr);
+}
