546a3b4d574876c57011472b617fb8d39bdbc47a
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-09-10 13:16:05 +0100

    fix normes of parsing

--- srcs/mandatory/parsing/get_color.c
+++ srcs/mandatory/parsing/get_color.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_color.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mait-all <mait-all@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 10:29:40 by mdahani           #+#    #+#             */
-/*   Updated: 2025/09/02 18:34:02 by mait-all         ###   ########.fr       */
+/*   Updated: 2025/09/10 13:03:47 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 static int	convert_rgb_to_int(int r, int g, int b)
 {
-    return (r << 16 | g << 8 | b);
+	return (r << 16 | g << 8 | b);
 }
 
 void	get_color(t_map *map)
 {
-	char **f_color, **c_color;
+	char	**f_color;
+	char	**c_color;
 
 	f_color = ft_split(map->f_color, ',');
 	c_color = ft_split(map->c_color, ',');
-
 	map->floor_color = convert_rgb_to_int(ft_atoi(f_color[0]),
-            ft_atoi(f_color[1]),ft_atoi(f_color[2]));
+			ft_atoi(f_color[1]), ft_atoi(f_color[2]));
 	map->ceiling_color = convert_rgb_to_int(ft_atoi(c_color[0]),
-            ft_atoi(c_color[1]),ft_atoi(c_color[2]));
+			ft_atoi(c_color[1]), ft_atoi(c_color[2]));
 }
