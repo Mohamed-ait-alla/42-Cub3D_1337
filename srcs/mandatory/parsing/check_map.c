7cc769c9f340a67ed94b5397bce4bb0271a53c4f
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-09-15 11:13:48 +0100

    fix checker of order map

--- srcs/mandatory/parsing/check_map.c
+++ srcs/mandatory/parsing/check_map.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 11:09:12 by mdahani           #+#    #+#             */
-/*   Updated: 2025/09/13 15:04:51 by mdahani          ###   ########.fr       */
+/*   Updated: 2025/09/15 10:41:20 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,6 +113,8 @@ bool	check_map(char *file, t_map *map)
 		return (false);
 	if (!get_map(map, file))
 		return (false);
+	if (check_order_of_map(file, map) != 6)
+		return (false);
 	get_px_py_and_normalize_map(map);
 	if (!is_map_closed(map))
 		return (false);
@@ -120,6 +122,5 @@ bool	check_map(char *file, t_map *map)
 		&& map->f_color && map->c_color
 		&& map->rows && map->num_no == 1 && map->num_so == 1 && map->num_we == 1
 		&& map->num_ea == 1 && map->num_f_color == 1 && map->num_c_color == 1
-		&& check_order_of_map(file, map) == 6 && check_num_of_players(map)
-		&& check_color(map));
+		&& check_num_of_players(map) && check_color(map));
 }
